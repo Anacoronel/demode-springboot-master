6570a88606001287d44d21b3d52e6f1eb3a0bfd8
Ana Coronel <anacoronel@hotmail.com>
2022-01-09 12:24:53 -0300

    agregado CORS

--- src/main/java/com/demo1/demo1/Controller/EduController.java
+++ src/main/java/com/demo1/demo1/Controller/EduController.java
@@ -10,6 +10,7 @@ import com.demo1.demo1.service.iEducacionService;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/educacion")
 
 public class EduController {
--- src/main/java/com/demo1/demo1/Controller/SkillController.java
+++ src/main/java/com/demo1/demo1/Controller/SkillController.java
@@ -9,6 +9,7 @@ import com.demo1.demo1.model.Skill;
 import com.demo1.demo1.service.iSkillService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/skill")
 
 
